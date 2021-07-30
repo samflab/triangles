ed576cc3beb5e592bf93fa77d62fd87892f2092d
Masudha Meher <kasturimeh@gmail.com>
2021-07-31 00:09:51 +0530

    design: hypotenuse page

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function openCity(evt, eventName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(eventName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(eventName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
 
-  document.getElementById("Angles").style.display = "block";
+      document.getElementById("Angles").style.display = "block";
